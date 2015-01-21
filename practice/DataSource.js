e200af49fe64a893e801ebbc0c2eaf1327c80e77
chenyue <15532196053@163.com>
2015-01-21 18:13:10 +0800

    编写测试

---
+++ practice/DataSource.js
@@ -0,0 +1,14 @@
+function get_completed_course()
+{
+    return [{course_name:'数学',score:82,pass_score:60,is_required:true,credit:4},
+        {course_name:'语文',score:80,pass_score:60,is_required:false,credit:3},
+        {course_name:'物理',score:95,pass_score:60,is_required:true,credit:2},
+        {course_name:'化学',score:77,pass_score:60,is_required:false,credit:2},
+        {course_name:'课程A',score:75,pass_score:60,is_required:true,credit:2},
+        {course_name:'课程B',score:69,pass_score:60,is_required:true,credit:2}];
+}
+function get_social_practice_course()
+{
+    return [{course_name:'实践A',score:88,pass_score:60,credit:2},
+        {course_name:'实践B',score:82,pass_score:60,credit:2}];
+}
---
+++ practice/ReplacementRule.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chenyu on 15/1/21.
+ */
+function ReplacementRule(arr_course_scores,arr_social_practice_scores)
+{
+    this.arr_course_scores = arr_course_scores;
+    this.arr_social_practice_scores = arr_social_practice_scores;
+}
