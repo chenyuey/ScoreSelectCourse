86548fcc43ad4af33b258f54ead5f289650289db
chenyue <15532196053@163.com>
2015-01-21 08:19:21 +0800

    添加测试文件

---
+++ spec/Replacement_Rule_Spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by chenyue on 15-1-20.
+ */
+describe('置换规则',function(){
+    it('哈蒙理工大学实践课不能置换已经修过的课程',function(){
+    });
+    it('南哈蒙理工大学实践课可以置换已经修过的课程',function(){
+    });
+});
