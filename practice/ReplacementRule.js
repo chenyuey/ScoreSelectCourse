97e4f3f671b8efb63702259c639cf5530a44c234
chenyue <15532196053@163.com>
2015-01-23 17:54:55 +0800

    添加文件

--- practice/ReplacementRule.js
+++ practice/ReplacementRule.js
@@ -6,4 +6,12 @@ function ReplacementRule(student_scores,type)
     this.student_scores = student_scores;
     this.type = type;
 }
-
+ReplacementRule.prototype.show_scores()
+{
+    var arr_social_practice_scores = this.student_scores.arr_social_practice_scores;
+    var arr_course_scores = this.student_scores.arr_course_scores;
+    if(this.type == 'hamon')
+    {
+        
+    }
+}
